81b12a682601bba6f75b74365fe99ba8144905ed
Matvey Ivanov <98405814+NissZZ@users.noreply.github.com>
2024-10-05 15:34:24 +0300

    Update №1.cpp

--- №1.cpp
+++ №1.cpp
@@ -10,34 +10,48 @@ int main()
     cin>>y;
     cout << "Enter the third number:";
     cin>>z;
+
+    
     if(x % 2 != 0 && y % 2 != 0)
     {
         cout<<"condition is true"<<endl;
     }
     else cout<<"condition is false"<<endl;
+
+    
     if((x < 20 && y >= 20) || (x >= 20 && y < 20))
     {
         cout<<"condition is true"<<endl;
     }
     else cout<<"condition is false"<<endl;
+
+    
     if(x == 0 || y == 0)
     {
         cout<<"condition is true"<<endl;
     }
     else cout<<"condition is false"<<endl;
+
+    
     if(x < 0 && y < 0 && z < 0)
     {
         cout<<"condition is true"<<endl;
     }
     else cout<<"condition is false"<<endl;
+
+        
     if((x % 5 == 0 && y % 5 != 0 && z % 5 != 0) || (x % 5 != 0 && y % 5 == 0 && z % 5 != 0) || (x % 5 != 0 && y % 5 != 0 && z % 5 == 0))
     {
         cout<<"condition is true"<<endl;
     }
     else cout<<"condition is false"<<endl;
+
+    
     if(x > 100 || y > 100 || z > 100)
     {
         cout<<"condition is true"<<endl;
     }
     else cout<<"condition is false"<<endl;
+
+    
 }
